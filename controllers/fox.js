0e08c60ad993eabc177c0b4ba7b2a6222a1ef248
Pranayreddybakaram <pranay.bakaram@gmail.com>
2021-11-17 20:55:36 -0600

    updated bonus

--- controllers/fox.js
+++ controllers/fox.js
@@ -132,3 +132,16 @@ exports.fox_update_Page =  async function(req, res) {
         res.send(`{'error': '${err}'}`); 
     } 
 };
+// Handle a delete one view with id from query 
+exports.fox_delete_Page = async function(req, res) { 
+    console.log("Delete view for id "  + req.query.id) 
+    try{ 
+        result = await fox.findById(req.query.id) 
+        res.render('foxdelete', { title: 'fox Delete', toShow: 
+result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
