c68701de3d1b584122abd0c67761e43dff380a50
Pranayreddybakaram <pranay.bakaram@gmail.com>
2021-11-30 16:43:08 -0600

    updated fox.js

--- models/fox.js
+++ models/fox.js
@@ -1,3 +1,3 @@
 const mongoose = require("mongoose") 
-const foxSchema = mongoose.Schema({ name: String, color: String, weight: Number }) 
+const foxSchema = mongoose.Schema({ name: String, color: String, weight:  {type:Number,min:15,max:100} }) 
 module.exports = mongoose.model("fox", foxSchema) 
--- routes/fox.js
+++ routes/fox.js
@@ -2,6 +2,13 @@ var express = require('express');
 const fox_controlers= require('../controllers/fox');
 
 var router = express.Router();
+const secured = (req, res, next) => { 
+    if (req.user){ 
+      return next(); 
+    } 
+    req.session.returnTo = req.originalUrl; 
+    res.redirect("/login"); 
+  } 
 
 
 /* GET home page. */
@@ -9,10 +16,10 @@ router.get('/', fox_controlers.fox_view_all_Page);
 /* GET detail fox page */ 
 router.get('/detail', fox_controlers.fox_view_one_Page); ; 
 /* GET create fox page */ 
-router.get('/create', fox_controlers.fox_create_Page); 
+router.get('/create',secured, fox_controlers.fox_create_Page); 
 /* GET create update page */ 
-router.get('/update', fox_controlers.fox_update_Page);
+router.get('/update',secured, fox_controlers.fox_update_Page);
 /* GET create fox page */ 
-router.get('/delete', fox_controlers.fox_delete_Page);
+router.get('/delete',secured, fox_controlers.fox_delete_Page);
 
 module.exports = router;
